04872662be154c80efe957d39f6b4a4d3d2e528d
mjgalindo <mjgalindo@unizar.es>
2019-11-14 15:46:57 +0100

    Fix python module

--- nlosbpy.cpp
+++ nlosbpy.cpp
@@ -53,7 +53,7 @@ inline xt::pyarray<float> backproject(
     }
 }
 
-inline xt::pyarray<float> phasor_reconstruction(
+inline xt::pyarray<std::complex<float>> phasor_reconstruction(
     const xt::pyarray<float> &transient_data,
     const xt::pyarray<float> &camera_grid_positions,
     const xt::pyarray<float> &laser_grid_positions,
@@ -65,34 +65,22 @@ inline xt::pyarray<float> phasor_reconstruction(
     const xt::pyarray<float> &volume_position,
     const xt::pyarray<float> &volume_size,
     const xt::pyarray<uint32_t> &voxels_per_side,
+    float wavelength,
     bool assume_row_major = false,
     bool use_cpu = false)
 {
-    if (use_cpu)
-    {
-        return bp::phasor_reconstruction(transient_data,
-                    camera_grid_positions,
-                    laser_grid_positions,
-                    camera_position,
-                    laser_position,
-                    t0, deltaT, is_confocal,
-                    volume_position,
-                    volume_size[0],
-                    voxels_per_side[0]);
-    }
-    else
-    {
-        return bp::gpu_backproject(transient_data,
-                    camera_grid_positions,
-                    laser_grid_positions,
-                    camera_position,
-                    laser_position,
-                    t0, deltaT, is_confocal,
-                    volume_position,
-                    volume_size,
-                    voxels_per_side,
-                    assume_row_major);
-    }
+    return bp::phasor_reconstruction(transient_data,
+                                     camera_grid_positions,
+                                     laser_grid_positions,
+                                     camera_position,
+                                     laser_position,
+                                     t0, deltaT, is_confocal,
+                                     volume_position,
+                                     volume_size[0],
+                                     voxels_per_side[0],
+                                     wavelength,
+                                     use_cpu, false,
+                                     assume_row_major);
 }
 
 // Python Module and Docstrings
